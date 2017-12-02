33461a52439ed57516e087f5f03fbf7abac39976
JamesOsborne <james.osborne112@gmail.com>
2017-12-02 16:11:00 -0500

    Add spikes to platforms that kill on contact

--- main/level/objects/platform.go
+++ main/level/objects/platform.go
@@ -14,7 +14,7 @@ components {
   }
 }
 embedded_components {
-  id: "sprite0"
+  id: "sprite"
   type: "sprite"
   data: "tile_set: \"/main/level/level.atlas\"\n"
   "default_animation: \"rock_planks\"\n"
@@ -47,7 +47,7 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: 190.0\n"
+  "      x: 0.0\n"
   "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
@@ -60,7 +60,7 @@ embedded_components {
   "    index: 0\n"
   "    count: 3\n"
   "  }\n"
-  "  data: 371.7635\n"
+  "  data: 188.978\n"
   "  data: 61.9855\n"
   "  data: 10.0\n"
   "}\n"
@@ -81,15 +81,179 @@ embedded_components {
   }
 }
 embedded_components {
-  id: "sprite1"
+  id: "left_spike"
   type: "sprite"
   data: "tile_set: \"/main/level/level.atlas\"\n"
-  "default_animation: \"rock_planks\"\n"
+  "default_animation: \"spikes\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
   position {
-    x: 373.0
+    x: -203.0
+    y: 0.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "right_spike"
+  type: "sprite"
+  data: "tile_set: \"/main/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 207.0
+    y: 5.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: -1.0
+    w: 6.123234E-17
+  }
+}
+embedded_components {
+  id: "bottom_left_spike"
+  type: "sprite"
+  data: "tile_set: \"/main/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: -91.0
+    y: -88.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "bottom_right_spike"
+  type: "sprite"
+  data: "tile_set: \"/main/level/level.atlas\"\n"
+  "default_animation: \"spikes\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 100.0
+    y: -88.0
+    z: -0.1
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.70710677
+    w: 0.70710677
+  }
+}
+embedded_components {
+  id: "danger_edges"
+  type: "collisionobject"
+  data: "collision_shape: \"\"\n"
+  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
+  "mass: 0.0\n"
+  "friction: 0.1\n"
+  "restitution: 0.5\n"
+  "group: \"danger\"\n"
+  "mask: \"hero\"\n"
+  "embedded_collision_shape {\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -91.0\n"
+  "      y: -88.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 0\n"
+  "    count: 3\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 100.0\n"
+  "      y: -88.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 3\n"
+  "    count: 3\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -203.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.70710677\n"
+  "      w: 0.70710677\n"
+  "    }\n"
+  "    index: 6\n"
+  "    count: 3\n"
+  "  }\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: 207.0\n"
+  "      y: 5.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.70710677\n"
+  "      w: 0.70710677\n"
+  "    }\n"
+  "    index: 9\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 65.162\n"
+  "  data: 25.0445\n"
+  "  data: 10.0\n"
+  "  data: 65.162\n"
+  "  data: 25.0445\n"
+  "  data: 10.0\n"
+  "  data: 65.162\n"
+  "  data: 25.0445\n"
+  "  data: 10.0\n"
+  "  data: 65.162\n"
+  "  data: 25.0445\n"
+  "  data: 10.0\n"
+  "}\n"
+  "linear_damping: 0.0\n"
+  "angular_damping: 0.0\n"
+  "locked_rotation: false\n"
+  ""
+  position {
+    x: 0.0
     y: 0.0
     z: 0.0
   }
