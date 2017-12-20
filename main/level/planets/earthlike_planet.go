c0e87bf15732801d9324b66bda06b49aed778c9e
JamesOsborne <james.osborne112@gmail.com>
2017-12-20 14:05:21 -0600

    Added parallaxing background and  rset ground and platform speed on reset

---
+++ main/level/planets/earthlike_planet.go
@@ -0,0 +1,20 @@
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/level/level.atlas\"\n"
+  "default_animation: \"earthlike_planet\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 75.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
